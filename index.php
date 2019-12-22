aecbafefb4478be13d2809599c2f95b35a42bf20
Mediashare <mediashare.supp@gmail.com>
2019-12-22 17:58:04 +0100

    Init Library

--- index.php
+++ index.php
@@ -4,4 +4,3 @@ require 'vendor/autoload.php';
 use Mediashare\Scraper\Scraper;
 $scraper = new Scraper("http://marquand.pro");
 $scraper->run();
-dump($scraper);die;
--- src/Entity/Result.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Mediashare\Scraper\Entity;
-
-Class Result
-{
-    public $url; // Url
-    public $scrape; // DomCrawler
-    public $webpage; // Webpage
-}
